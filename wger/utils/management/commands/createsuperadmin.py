feefb4c36867f018b47078541373889b6e1c176e
eugenius <eudriku@gmail.com>
2018-06-01 19:09:33 +0300

    Add management command

--- wger/utils/management/commands/createsuperadmin.py
+++ wger/utils/management/commands/createsuperadmin.py
@@ -6,7 +6,8 @@ class Command(BaseCommand):
     help = 'Crate a superuser, and allow password to be provided'
 
     def handle(self, *args, **options):
-        u = User(username='admin')
+        u = User.objects.get_or_create(username='admin')
+
         u.is_superuser = True
         u.is_staff = True
         u.save()
